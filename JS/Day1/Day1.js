ba24a07d2a2b1f9ac769fa4a37f523c3df5a9742
SudharsanMaruthaiyan <sudharsan6078@gmail.com>
2025-10-09 22:36:01 +0530

    JS Day - 1

---
+++ JS/Day1/Day1.js
@@ -0,0 +1,85 @@
+// var = > Global Scope
+// let = > Blocked Scope
+// const = > Blocked Scope & Immutable
+
+// var a = "var keyword";
+// a = "changed"
+// {
+//   console.log(a);
+// }
+
+// {
+//   let a = "let keyword";
+//   a = "changed";
+// }
+// console.log(a);
+
+// {
+//   const a = "const keyword";
+//   a = "changed";
+//   console.log(a);
+// }
+
+// data  ( ctl + /)
+
+/**** */
+
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(10 / 5);
+// console.log(10 % 5);
+
+// pre increament    ++a
+// post increament   a++
+
+// pre increament
+// var a = 5;
+// console.log(++a);
+
+// post increament
+// var a = 5;
+// console.log(a++);
+// console.log(a);
+
+// Primitive Datatype
+// number , string , boolean , null , undefined
+
+// var a = 8; // number
+// var b = "name"; // string
+// var c = false; // boolean
+// var d = null; // object
+// var e; // undefined
+// console.log(typeof e);
+
+// function
+// Declarative function
+
+// function add() {
+//   console.log("Declarative function");
+// }
+// add();
+
+// parameter function
+// function add(a, b) {
+//   // parameter
+//   console.log(a + b);
+// }
+
+// add(5, 3); // argument  8
+// add(5, 5); // argument  10
+
+// condition  statement
+// if , else
+// if => true
+// else => flase
+
+// function calculate(age) {
+//   if (age > 19) {
+//     console.log("yes");
+//   } else {
+//     console.log("no");
+//   }
+// }
+
+// calculate(20);
